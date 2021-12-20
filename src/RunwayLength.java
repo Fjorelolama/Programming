3d1703a605ab56dbf2e9112634f51d75ab2cf970
Fjorelolama <82113469+Fjorelolama@users.noreply.github.com>
2021-12-20 14:17:57 +0100

    Initial commit

---
+++ src/RunwayLength.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RunwayLength {
+    public static void main(String[] args) {
+        double v_meter_second;
+        double a_meter_second;
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Enter speed and acceleration: ");
+
+        v_meter_second = userInput.nextDouble();
+        a_meter_second = userInput.nextDouble();
+
+         double length =(v_meter_second * v_meter_second) / (2 * a_meter_second);
+
+        System.out.println("The minimum runway length for this airplane is: " + length);
+    }
+}
